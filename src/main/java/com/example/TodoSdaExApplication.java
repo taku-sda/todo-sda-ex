d5b1a0c50afa01fea7262f86d595b7f7f0a5a356
taku-sda <t.suda.2196@gmail.com>
2020-10-05 14:19:22 +0900

    初回コミット

---
+++ src/main/java/com/example/TodoSdaExApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSdaExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSdaExApplication.class, args);
+	}
+
+}
