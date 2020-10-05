4c027b669f7261f6847d66db5fb9a5cc4766b870
taku-sda <t.suda.2196@gmail.com>
2020-10-05 16:29:27 +0900

    [ADD]DBにテストユーザーのデータを追加するdata.sqlを作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+--テストユーザーのデータが存在しない場合は追加する
+INSERT INTO usr(user_id, password, role_name) 
+SELECT "testuser", "$2a$10$SWXUVkBsmWH3sXEU5YNlt.wDVMEcQpPGKA0PP4Z9uTRU2IPj69Zxm", "TEST"
+WHERE 
+  NOT EXISTS(
+    SELECT user_id FROM usr WHERE user_id= "testuser"
+    );
