8fce3ebefa30303de625024205180b5a0bd476ef
taku-sda <t.suda.2196@gmail.com>
2020-10-06 14:31:31 +0900

    [Fix]パスの誤りを修正

--- src/main/java/com/example/app/login/LoginController.java
+++ src/main/java/com/example/app/login/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("loginForm")
+@RequestMapping("/loginForm")
 public class LoginController {
   public String loginForm(Model model) {
     model.addAttribute("title", "ToDo!! | ログインフォーム");
