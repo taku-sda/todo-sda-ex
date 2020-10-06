35fb68f7224948f43121248030308302dc5c2b9a
taku-sda <t.suda.2196@gmail.com>
2020-10-06 14:45:57 +0900

    [Fix]LoginController
    @GetMappingの付け忘れを修正

--- src/main/java/com/example/app/login/LoginController.java
+++ src/main/java/com/example/app/login/LoginController.java
@@ -2,11 +2,13 @@ package com.example.app.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/loginForm")
 public class LoginController {
+  @GetMapping
   public String loginForm(Model model) {
     model.addAttribute("title", "ToDo!! | ログインフォーム");
     return "login/loginForm";
