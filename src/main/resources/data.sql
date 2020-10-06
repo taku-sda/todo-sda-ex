e11d13847229c19dd25e56775f9bc55dd57dc747
taku-sda <t.suda.2196@gmail.com>
2020-10-06 16:10:44 +0900

    [Modeify]テストユーザーのパスワードを変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 --テストユーザーのデータが存在しない場合は追加する
 INSERT INTO usr(user_id, password, role_name) 
-SELECT 'testuser', '$2a$10$SWXUVkBsmWH3sXEU5YNlt.wDVMEcQpPGKA0PP4Z9uTRU2IPj69Zxm', 'TEST'
+SELECT 'testuser', '$2a$10$uCwUK7lhZ1Xr4kg1kWfhD.ZoIaVNbxelFthfNQEMhj3.5IqMmAyfq', 'TEST'
 WHERE 
   NOT EXISTS(
     SELECT user_id FROM usr WHERE user_id= 'testuser'
