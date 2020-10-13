ba7bdfaf0b7e5d09b133a0de53bdaafa43e6dbb0
taku-sda <t.suda.2196@gmail.com>
2020-10-13 13:09:04 +0900

    [Fix]@Transactionalの記入漏れを修正

--- src/main/java/com/example/domain/service/user/UserRegisterServiceImpl.java
+++ src/main/java/com/example/domain/service/user/UserRegisterServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.model.User;
 import com.example.domain.repository.user.UserRepository;
@@ -19,6 +20,7 @@ public class UserRegisterServiceImpl implements UserRegisterService {
   UserRepository repository;
   
   @Override
+  @Transactional(readOnly = true)
   public void confirmAvailability(String userId) {
     Optional<User> existUser = repository.findById(userId);
     if(existUser.isPresent()) {
@@ -27,6 +29,7 @@ public class UserRegisterServiceImpl implements UserRegisterService {
   }
 
   @Override
+  @Transactional
   public User register(User registerUser) {
     PasswordEncoder encoder = new BCryptPasswordEncoder();
     String hashedPassword = encoder.encode(registerUser.getPassword());
