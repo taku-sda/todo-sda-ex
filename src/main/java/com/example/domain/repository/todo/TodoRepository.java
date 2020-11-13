7a9ce128451152ecb72e83d82ef69b58ef9f4a13
taku-sda <t.suda.2196@gmail.com>
2020-11-13 14:58:57 +0900

    [Add]リポジトリクラスにTodo一覧取得のメソッドの実装。カスタムリポジトリクラスを作成し、Todo一覧取得のメソッドを実装

--- src/main/java/com/example/domain/repository/todo/TodoRepository.java
+++ src/main/java/com/example/domain/repository/todo/TodoRepository.java
@@ -1,11 +1,35 @@
 package com.example.domain.repository.todo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.domain.model.Todo;
 
 /**
  * Todoエンティティに対応したJpaRepository.
  */
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
+public interface TodoRepository extends JpaRepository<Todo, Integer>, TodoRepositoryCustom {
+
+  /**
+   * ユーザーに対応する今日中のToDoのリストを取得する.
+   * 期限の日付が今日と一致して、完了状態でないレコードのリスト.
+   * @param userId リストを取得するユーザーのID
+   * @return ToDoのリスト
+   */
+  @Query("SELECT t FROM Todo t WHERE t.user.userId = :userId "
+      + "AND CAST(t.deadline AS date) = CURRENT_DATE AND t.completed = FALSE")
+  List<Todo> getTodayList(@Param("userId") String userId);
+
+  /**
+   * ユーザーに対応する期限切れのToDoのリストを取得する.
+   *  期限が過ぎており、完了状態でないレコードのリスト
+   * @param userId  リストを取得するユーザーのID
+   * @return  ToDoのリスト
+   */
+  @Query("SELECT t FROM Todo t WHERE t.user.userId = :userId "
+      + "AND t.deadline < CURRENT_TIMESTAMP AND t.completed = FALSE")
+  List<Todo> getExpiredList(@Param("userId") String userId);
 }
---
+++ src/test/java/com/example/domain/repository/todo/TodoRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.example.domain.repository.todo;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.domain.model.RoleName;
+import com.example.domain.model.Todo;
+import com.example.domain.model.User;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@DisplayName(" TodoRepositoryの単体テスト")
+class TodoRepositoryTest {
+
+  @Autowired
+  TestEntityManager entityManager;
+
+  @Autowired
+  TodoRepository repository;
+
+  User user = new User("user", "password", RoleName.TEST);
+  User wrongUser = new User("wrongUser", "password", RoleName.TEST);
+  
+  @BeforeEach
+  void setUp() {
+    entityManager.persist(user);
+    entityManager.persist(wrongUser);
+  }
+
+ @Test
+ @DisplayName("getTodayList()のテスト")
+ void getTodayListTest() {
+   LocalDateTime now = LocalDateTime.now();
+   entityManager.persist(new Todo(null, user, "レコード1", now, 1, "", false, now));
+   entityManager.persist(new Todo(null, wrongUser, "レコード2", now, 1, "", false, now)); //ユーザーが異なる
+   entityManager.persist(new Todo(null, user, "レコード3", now, 1, "", true, now)); //完了状態
+   entityManager.persist(new Todo(null, user, "レコード4", now.plusDays(1), 1, "", false, now));  //期限が翌日
+   entityManager.persist(new Todo(null, user, "レコード5", now.minusDays(1), 1, "", false, now)); //期限が前日
+   
+   List<Todo> resultList = repository.getTodayList(user.getUserId());
+   assertThat(resultList.size(), is(1));
+   assertThat(resultList.get(0).getTitle(), is("レコード1"));
+ }
+
+ @Test
+ @DisplayName("getExpiredList()のテスト")
+ void getExpiredListTest() {
+   LocalDateTime now = LocalDateTime.now();
+   entityManager.persist(new Todo(null, user, "レコード1", now.minusDays(1), 1, "", false, now));
+   entityManager.persist(new Todo(null, wrongUser, "レコード2", now, 1, "", false, now)); //ユーザーが異なる
+   entityManager.persist(new Todo(null, user, "レコード3", now, 1, "", true, now)); //完了状態
+   entityManager.persist(new Todo(null, user, "レコード4", now.plusDays(1), 1, "", false, now));  //期限を過ぎていない
+
+   List<Todo> resultList = repository.getExpiredList(user.getUserId());
+   assertThat(resultList.size(), is(1));
+   assertThat(resultList.get(0).getTitle(), is("レコード1"));
+ }
+}
