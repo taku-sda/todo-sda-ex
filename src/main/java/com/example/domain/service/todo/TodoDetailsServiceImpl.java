25c27c53921f6376f521fcd98835517cf25da91b
taku-sda <t.suda.2196@gmail.com>
2021-02-09 16:11:34 +0900

    [Add]サービスクラスを作成

---
+++ src/main/java/com/example/domain/service/todo/TodoDetailsServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.domain.service.todo;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.model.Todo;
+import com.example.domain.repository.todo.TodoRepository;
+
+/**
+ * 既存のTodoの登録内容の詳細や変更に関する処理を行うサービスクラス.
+ */
+@Service
+public class TodoDetailsServiceImpl implements TodoDetailsService {
+
+  @Autowired
+  TodoRepository repository;
+
+  /** {@inheritDoc} */
+  @Override
+  @Transactional(readOnly = true)
+  public Todo getTodo(Integer todoId) {
+    Optional<Todo> optionalTodo = repository.findById(todoId);
+    if (optionalTodo.isEmpty()) {
+      throw new IllegalArgumentException("指定されたTodoは存在しません。");
+    }
+
+    return optionalTodo.get();
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  @Transactional
+  public void updateDetails(Integer todoId, String title, LocalDateTime deadline, Integer priority,
+      String memo) {
+    Todo updateTodo = getTodo(todoId);
+    updateTodo.setTitle(title);
+    updateTodo.setDeadline(deadline);
+    updateTodo.setPriority(priority);
+    updateTodo.setMemo(memo);
+  }
+
+}
