d787ea54eeed4fcdb069a774214febf972129782
taku-sda <t.suda.2196@gmail.com>
2021-06-30 04:24:02 +0900

    [Modify]ボタンのデザイン、挙動を変更
    ボタンのアイコンを変更
    ボタンを押した際に、スクロールでトップに移動するように変更
    ある程度画面をスクロールしたら、画面下部に追従するボタンが出現するように変更

---
+++ src/main/resources/static/js/layout.js
@@ -0,0 +1,15 @@
+$(function(){
+  var pagetop = $('#page_top');
+  pagetop.hide();
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 400) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+  });
+});
