0c642e440a161aa58627b15a3a5eed39c987806f
taku-sda <t.suda.2196@gmail.com>
2021-07-15 03:55:58 +0900

    [Modify]ページ最下部位置の取得方法を変更

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -20,7 +20,7 @@ $(function(){
   var bottommenu = $('#bottom_menu');
   $(window).scroll(function () {
     /* ページ最下部の位置を取得 */
-    var bottom = $(document).innerHeight() - $(window).innerHeight(); 
+    var bottom = $(document).innerHeight() - document.documentElement.clientHeight; 
     
     /* ページ最下部より30px上部に到達でボトムメニューを非表示にする */
     if (bottom - 30 <= $(window).scrollTop()) {
